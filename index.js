1c77243de134f468cd3fbbebccdee8f50c141442
MimFSD <mimislam878@gmail.com>
2024-08-07 00:19:10 +0600

    complete mongo db connection and get api

--- index.js
+++ index.js
@@ -27,7 +27,9 @@ const client = new MongoClient(uri, {
 //https://i.ibb.co/bm32RsT/img20240427-12245804.png
 async function run() {
     try {
-                
+        // Connect the client to the server	(optional starting in v4.7)
+        // await client.connect();
+
         // Create database and collection
         const craftsCollection = client.db("craftsDB").collection("crafts");
         const categoriesCollection = client.db("categoriesDB").collection("categories")
@@ -40,6 +42,7 @@ async function run() {
             res.send(result);
         })
 
+        // for id fetching
         app.get('/addcrafts/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) };
@@ -84,7 +87,7 @@ async function run() {
             const newCrafts = req.body;
             console.log(newCrafts);
             const result = await craftsCollection.insertOne(newCrafts);
-            res.send(result);   
+            res.send(result);   // result send to Database
         })
 
         // Delete Craft from database
